be5ed7e8fd67045aa3a24001e208a7847556f2e6
BigMoistLochu <krok.konrad1@gmail.com>
2023-10-29 23:06:29 +0100

    upload projekct

---
+++ src/test/java/com/example/listjobsproject/ListJobsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.listjobsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListJobsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
