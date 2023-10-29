0f65b43c0ec556aa28c554174216ae226ddd23dc
BigMoistLochu <krok.konrad1@gmail.com>
2023-10-29 23:06:02 +0100

    upload projekct

---
+++ src/main/java/com/example/listjobsproject/ListJobsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.listjobsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListJobsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListJobsProjectApplication.class, args);
+	}
+
+}
