93051d7b21e74a620ceb5722c5756e06abf80ad9
BigMoistLochu <krok.konrad1@gmail.com>
2023-11-03 20:12:23 +0100

    create entity PageJob

--- src/main/java/com/example/listjobsproject/models/PageJob.java
+++ src/main/java/com/example/listjobsproject/models/PageJob.java
@@ -1,7 +1,70 @@
 package com.example.listjobsproject.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.math.BigDecimal;
+
+@Entity
 public class PageJob {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String url;
+    private BigDecimal salary;
+    private String company;
 
+    public PageJob() {}
 
+    public PageJob(String title, String url, BigDecimal salary, String company) {
+        this.title = title;
+        this.url = url;
+        this.salary = salary;
+        this.company = company;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public BigDecimal getSalary() {
+        return salary;
+    }
+
+    public void setSalary(BigDecimal salary) {
+        this.salary = salary;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
 }
