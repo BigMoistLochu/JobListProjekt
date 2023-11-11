84aaee44a89aaf7be14e958a26349ab1f32bf81d
BigMoistLochu <krok.konrad1@gmail.com>
2023-11-11 15:49:27 +0100

    prepare and founding cssQuerySelector in HTML file from pracujPL,

--- src/main/java/com/example/listjobsproject/scrapper/PracujPl.java
+++ src/main/java/com/example/listjobsproject/scrapper/PracujPl.java
@@ -8,11 +8,13 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.jsoup.nodes.Element;
 
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ListIterator;
 
 public class PracujPl implements Runnable, EventListener {
 
@@ -28,7 +30,19 @@ public class PracujPl implements Runnable, EventListener {
 
         try
         {
-            Document doc = Jsoup.connect("http://pracuj.com").get();
+            Document doc = Jsoup.connect("https://it.pracuj.pl/praca/java;kw?et=1%2C17&itth=38").get();
+
+            String company = doc.select(".core_c1sbal7t > div:nth-child(3) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > div:nth-child(2) > div:nth-child(1) > a:nth-child(1) > h4:nth-child(1)").text();
+            System.out.println(company);
+            String href = doc.select(".core_c1sbal7t > div:nth-child(3) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > a:nth-child(1)").attr("href");
+            System.out.println(href);
+            String title = doc.select(".core_c1sbal7t > div:nth-child(3) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > h2:nth-child(2) > a:nth-child(1)").text();
+            String title2 = doc.select(".core_c1sbal7t > div:nth-child(3) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > h2:nth-child(1)").text();
+            String title3 = doc.select(".core_c1sbal7t > div:nth-child(3) > div:nth-child(4) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > h2:nth-child(3) > a:nth-child(1)").text();
+            System.out.println(title);
+
+
+
             List<PageJobDto> listOfNoobs = new ArrayList<>(List.of(new PageJobDto("pracuje0","xd2",new BigDecimal("4334"),"xd3"),
                 new PageJobDto("pracuje1","xd2",new BigDecimal("4334"),"xd3"),
                 new PageJobDto("pracuje2","xd2",new BigDecimal("4334"),"xd3"),
